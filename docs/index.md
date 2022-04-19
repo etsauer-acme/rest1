37e34717f6b11af30edc0b7025d6f6ee1b29133d
Scaffolder <scaffolder@backstage.io>
2022-04-19 15:11:28 +0000

    Initial commit

---
+++ docs/index.md
@@ -0,0 +1,7 @@
+# "eric1-rest"
+
+"Testing an application"
+
+## Getting started
+
+Start by saying Hello, someone will respond saying Hi.
---
+++ src/test/java/io/io.etsauer-acme.foo/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package io.io.etsauer-acme.foo;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
